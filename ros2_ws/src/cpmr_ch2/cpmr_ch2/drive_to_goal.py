a30705b3a7b6ffe5b217eec387811253e8dd0058
Christopher Budd <cbudd@adventuregang.ca>
2024-11-13 22:24:01 -0500

    test

--- ros2_ws/src/cpmr_ch2/cpmr_ch2/drive_to_goal.py
+++ ros2_ws/src/cpmr_ch2/cpmr_ch2/drive_to_goal.py
@@ -1,4 +1,5 @@
 import math
+from math import atan2
 import numpy as np
 import rclpy
 from rclpy.node import Node
@@ -7,6 +8,21 @@ from rcl_interfaces.msg import SetParametersResult
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist, Pose, Point, Quaternion
 from nav_msgs.msg import Odometry
+import cv2
+import random
+from scipy.spatial import KDTree
+import time
+list12 = []
+numberOfNodes = 1000
+map = cv2.imread('map.jpg')
+
+purple = (86,70,115)
+black = (38,22,27)
+darkBlue = (64,38,42)
+orange = (68,149,242)
+red = (70,78,166)
+
+
 
 def euler_from_quaternion(quaternion):
     """
@@ -37,68 +53,118 @@ class MoveToGoal(Node):
         super().__init__('move_robot_to_goal')
         self.get_logger().info(f'{self.get_name()} created')
 
-        self.declare_parameter('goal_x', 0.0)
-        self._goal_x = self.get_parameter('goal_x').get_parameter_value().double_value
-        self.declare_parameter('goal_y', 0.0)
-        self._goal_y = self.get_parameter('goal_y').get_parameter_value().double_value
-        self.declare_parameter('goal_t', 0.0)
-        self._goal_t = self.get_parameter('goal_t').get_parameter_value().double_value
+        self.cylinderList = []
+
+        self._cur_x = 0.0
+        self._cur_y= 0.0
+        self._goal_x = 24.0
+        self._goal_y = 24.0
+        self._goal_t = 0.0
+        self.max_vel = 0.2
+        self.max_gain = 5.0
+        self.newGoal= "0.0&0.0"
+        self.createMap = "a"
+
         self.add_on_set_parameters_callback(self.parameter_callback)
-        self.get_logger().info(f"initial goal {self._goal_x} {self._goal_y} {self._goal_t}")
+        self.declare_parameter('goal_x', value=self._goal_x)
+        self.declare_parameter('goal_y', value=self._goal_y)
+        self.declare_parameter('goal_t', value=self._goal_t)
+        self.declare_parameter('max_vel', value=self.max_vel)
+        self.declare_parameter('max_gain', value=self.max_gain)
+        self.declare_parameter('newGoal', value=self.newGoal)
+        self.max_vel = 3.0
+        self.max_gain = 5.0
 
         self._subscriber = self.create_subscription(Odometry, "/odom", self._listener_callback, 1)
         self._publisher = self.create_publisher(Twist, "/cmd_vel", 1)
-
-
-    def _listener_callback(self, msg, vel_gain=5.0, max_vel=0.2, max_pos_err=0.05):
+ 
+    def _listener_callback(self, msg, vel_gain=5.0, max_vel=5.2, max_pos_err=0.05):
+       
+        vel_gain = self.max_gain
+        max_vel = self.max_vel
         pose = msg.pose.pose
-
         cur_x = pose.position.x
         cur_y = pose.position.y
+
+        self._cur_x = cur_x
+        self._cur_y = cur_y
         o = pose.orientation
         roll, pitchc, yaw = euler_from_quaternion(o)
         cur_t = yaw
-        
+   
+
         x_diff = self._goal_x - cur_x
         y_diff = self._goal_y - cur_y
         dist = math.sqrt(x_diff * x_diff + y_diff * y_diff)
 
+        angleToGoal = atan2(y_diff,x_diff)
         twist = Twist()
-        if dist > max_pos_err:
-            x = max(min(x_diff * vel_gain, max_vel), -max_vel)
-            y = max(min(y_diff * vel_gain, max_vel), -max_vel)
+
+
+        if dist > max_pos_err: 
+
+            vector = [x_diff * vel_gain, y_diff * vel_gain]
+
+            mag = math.sqrt(vector[0]**2 + vector[1]**2 )
+            if mag > max_vel:
+                vector[0] /= mag
+                vector[1] /= mag
+
+                vector[0] *= max_vel
+                vector[1] *= max_vel
+
+            x = vector[0]
+            y = vector[1]
+
             twist.linear.x = x * math.cos(cur_t) + y * math.sin(cur_t)
             twist.linear.y = -x * math.sin(cur_t) + y * math.cos(cur_t)
-            self.get_logger().info(f"at ({cur_x},{cur_y},{cur_t}) goal ({self._goal_x},{self._goal_y},{self._goal_t})")
         self._publisher.publish(twist)
 
     def parameter_callback(self, params):
-        self.get_logger().info(f'move_robot_to_goal parameter callback {params}')
+        self.get_logger().info(f'move_robot_to_goal parameter callback')
+        changedGoal = False
         for param in params:
-            self.get_logger().info(f'move_robot_to_goal processing {param.name}')
             if param.name == 'goal_x' and param.type_ == Parameter.Type.DOUBLE:
                 self._goal_x = param.value
+                changedGoal = True
             elif param.name == 'goal_y' and param.type_ == Parameter.Type.DOUBLE:
                 self._goal_y = param.value
+                changedGoal = True
             elif param.name == 'goal_t' and param.type_ == Parameter.Type.DOUBLE:
                 self._goal_t = param.value
+                changedGoal = True
+            elif param.name == 'max_vel' and param.type_ == Parameter.Type.DOUBLE:
+                self.max_vel = param.value
+            elif param.name == 'max_gain' and param.type_ == Parameter.Type.DOUBLE:
+                self.max_gain = param.value
+            elif param.name == 'newGoal' and param.type_ == Parameter.Type.STRING:
+                self.get_logger().warn(f'NEW GOAL IS WORKING S')
+                self._goal_x = float(param.value.split("&")[0])
+                self._goal_y = float(param.value.split("&")[1])
+                changedGoal = True
+            
             else:
-                self.get_logger().warn(f'{self.get_name()} Invalid parameter {param.name}')
+                self.get_logger().warn(f'Invalid parameter {param.name}')
                 return SetParametersResult(successful=False)
-            self.get_logger().warn(f"Changing goal {self._goal_x} {self._goal_y} {self._goal_t}")
+           
+            if changedGoal:
+                self.get_logger().warn(f"Changing goal {self._goal_x} {self._goal_y} {self._goal_t}")
+            else:
+                self.get_logger().warn(f"Max Velocity Change: {self.max_vel} Max gain changed: {self.max_gain}")
         return SetParametersResult(successful=True)
 
-
-
+import os
+import cv2
 def main(args=None):
+   
     rclpy.init(args=args)
     node = MoveToGoal()
+   
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
         pass
     rclpy.shutdown()
-
 if __name__ == '__main__':
     main()
 
---
+++ ros2_ws/src/cpmr_ch2/cpmr_ch2/rrt_map.py
@@ -0,0 +1,200 @@
+import cv2
+import numpy as np
+import math
+import random
+import subprocess
+import time
+from scipy.spatial import KDTree
+
+
+
+
+class point:
+    def __init__(self, x, y,next = None, prev = None,radius = 1):
+        self.x = x
+        self.y = y
+        self.next = next
+        self.prev = prev
+        self.radius = radius
+        self.color = (127,127,0)
+        self.map = cv2.imread(fileName)
+
+
+
+def point_obst_overlap(map, p):
+    def is_not_free(x, y):
+        overlap = False
+        if all(map[y, x] == [0, 0, 0]):
+            overlap = True
+        return overlap
+
+    mapx, mapy = p.x, p.y
+
+    if is_not_free(mapx, mapy):
+        return True    
+    return False
+
+def line_color_intersection(map, v1, v2):
+    def is_not_white(x, y):
+        lineOverlap = False
+        if all(map[y, x] == [0, 0, 0]):
+            lineOverlap = True
+        return lineOverlap 
+    x1, y1, x2, y2 = v1.x, v1.y, v2.x, v2.y
+    dx, dy = abs(x2 - x1), abs(y2 - y1)
+    x, y = x1, y1
+    sx = -1 if x1 > x2 else 1
+    sy = -1 if y1 > y2 else 1
+    err = dx - dy
+    while True:
+        if x == x2 and y == y2:
+            break
+        e2 = 2 * err
+        if e2 > -dy:
+            err -= dy
+            x += sx
+        if e2 < dx:
+            err += dx
+            y += sy
+        if is_not_white(x, y):
+            return True
+    return False
+
+def findDistace (p1, p2):
+    distance = math.sqrt((p1.x - p2.x)**2 + (p2.y - p1.y)**2)
+    return distance
+
+def drawLine(v1,v2,color =(128,0,128), thickness=2):
+    cv2.line(map, (v1.x,v1.y), (v2.x,v2.y), color, thickness)
+
+
+def bigBrainAlgo(exploredVertexList,listOfVertix):
+    newConnection = 0 # This will be two different vertexes we will be returning
+    smallestDistance = float('inf')
+    for exploredV in exploredVertexList:
+        for unexploredVertix in listOfVertix:
+            calculateDistance = findDistace(exploredV,unexploredVertix)
+            if calculateDistance < smallestDistance and line_color_intersection(map, exploredV, unexploredVertix) == False:
+                smallestDistance = calculateDistance
+                newConnection = (exploredV,unexploredVertix)
+
+    return newConnection
+
+def findClosestNodeToGraph(exploredVertexList, listOfVertix):
+    unexploredPoints = [(v.x, v.y) for v in listOfVertix]
+
+    tree = KDTree(unexploredPoints)
+
+    smallestDistance = float('inf')
+    newConnection = None
+
+    for exploredV in exploredVertexList:
+        distance, index = tree.query((exploredV.x, exploredV.y))
+
+        if distance < smallestDistance and line_color_intersection(map, exploredV, listOfVertix[index]) == False:
+
+            smallestDistance = distance
+            newConnection = (exploredV, listOfVertix[index])
+            print('NEW CONNECTION MADE')
+        
+    
+        if distance == 0:
+            break
+    if newConnection == None:
+        newConnection = bigBrainAlgo(exploredVertexList,listOfVertix)
+    return newConnection
+
+def buildMap():
+    map = cv2.imread("NewFloorPlanEddited.jpg")
+    print(map)
+    randomPoints = [(np.random.randint(10, map.shape[1]-10), np.random.randint(10, map.shape[0]-10)) for _ in range(numberOfNodes)]
+    listOfVertix = []
+    for i in range(0,numberOfNodes):
+        v = point(x = randomPoints[i][0], y = randomPoints[i][1])
+        if point_obst_overlap(map,v):
+            v.color = (0, 255, 255) 
+        else: 
+            listOfVertix.append(v)
+        cv2.circle(map, (v.x,v.y), v.radius, v.color, thickness=-1)
+
+    cv2.imwrite("_PrelineMap"+ fileName ,map)
+    return listOfVertix
+
+def mainRead(start_x = 250, start_y = 250, finish_x = 15, finish_y = 15):
+    
+    listOfVertix = buildMap()
+
+    exploredVertexList = []
+
+    rrt = []
+    startVertex = point(start_x, start_y)
+    
+    random_index = random.randint(0, len(listOfVertix))
+
+    finishPoint = point(finish_x, finish_y)
+    finishPoint.color=(255, 255, 0)
+    
+    cv2.circle(map, (startVertex.x,startVertex.y), 6, (0,255,0), thickness=-1)
+    cv2.circle(map, (finishPoint.x,finishPoint.y), 6, (255,255,0), thickness=-1)
+
+    listOfVertix.insert(random_index, finishPoint)
+    exploredVertexList.append(startVertex)
+
+    while(len(listOfVertix) > 0):
+
+        graphNode, newNode = findClosestNodeToGraph(exploredVertexList, listOfVertix)
+
+        graphNode.next = newNode
+        newNode.prev = graphNode
+        drawLine(graphNode, newNode) 
+        exploredVertexList.append(newNode)
+        listOfVertix.remove(newNode)
+
+
+        print('ELEMENTS IN LIST OF VERTIX: ', len(listOfVertix))
+        print('GRAPH NODE: ', graphNode.x, graphNode.y)
+        print('NEW NODE: ', newNode.x, newNode.y)
+        print('*******NODE ADDED TO RRT*******')
+
+        # check if we have reached the goal            
+        if newNode.x == finishPoint.x and newNode.y == finishPoint.y:
+            print('FINISH POINT REACHED. BREAK OUT OF LOOP')
+            break
+        
+        print('\n')  
+    while(newNode.prev != None):
+        pointInX = (newNode.x/10.0)
+        pointInY = 63.5- (newNode.y/10.0)
+        rrt.append((pointInX,pointInY))
+        print(f"Location: {newNode.x} , {newNode.y}")
+        drawLine(newNode, newNode.prev,(0, 0, 255), 4) 
+        newNode = newNode.prev
+
+    cv2.imwrite("_output_image"+fileName,map)
+
+def movingInRos(listOfNodes):
+
+    command = "" 
+    listOfNodes.reverse()
+    
+    with open('wayPoints.txt', 'w') as file:
+        for r in listOfNodes:
+            file.write(f'{r}\n')
+
+    print("\n\n\nWORKING ON MOVEMENT \n\n\n")
+    for i in listOfNodes:
+        print(f"{i[0]}, {i[1]}")
+        command = f"ros2 param set /drive_to_goal newGoal \"{i[0]} & {i[1]}\"" 
+        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        print("Errors:", result.stderr)
+        print("Return code:", result.returncode)
+        print("Output:", result.stdout)
+        time.sleep(1.0)
+
+
+numberOfNodes = 1500
+fileName = "house-plan.png"
+map = cv2.imread(fileName)
+listOfNodes = mainRead(start_x=150,start_y=500,finish_x=600,finish_y=350)
+x = input("____________________\n PRESS TO CONINTUE \n________________\n")
+movingInRos(listOfNodes)
